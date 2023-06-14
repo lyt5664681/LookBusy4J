785f5781c89fd0f19cab1adb152a9090b29c1af8
lyt5664681 <lyt5664681@163.com>
2023-06-14 16:14:36 +0800

    #DCAWCL-6 [FIX] 取消maven启动测试

--- src/test/java/com/primeton/lookbusy4j/LookBusy4JApplicationTests.java
+++ src/test/java/com/primeton/lookbusy4j/LookBusy4JApplicationTests.java
@@ -3,10 +3,8 @@ package com.primeton.lookbusy4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LookBusy4JApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
